49170f7dee7d7b56379e2ebc677d00eef6fed13b
Shazaniyu55 <shazaniyu@gmail.com>
2024-07-11 14:40:39 +0100

    main

---
+++ src/components/hero.js
@@ -0,0 +1,77 @@
+import React from "react";
+import {motion} from 'framer-motion';
+
+
+
+const Heading = ({ subtitle, title }) => {
+    return (
+      <>
+        <div id='heading'>
+          <motion.h3 
+          initial={{x: -1000}}
+          animate={{x: 0}}
+          transition={{
+            duration: "2",
+          }}
+          className="text-2xl text-orange-500 font-bold">{subtitle} </motion.h3>
+          <h1 className="text-2xl text-orange-500 font-bold">{title} </h1>
+        </div>
+        
+      </>
+    )
+  }
+  
+
+
+const Hero = () => {
+  return (
+    <>
+      <section className='md:bg-cover md:bg-fixed bg-center pt-20 md:pt-30 h-screen relative flex justify-center items-center'>
+        <div className='w-full h-12'>
+        <video
+autoPlay
+  loop
+  muted
+  playsInline
+  className='background-video'
+  style={{
+    position: 'absolute',
+    top: '10%',
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+    zIndex: -1,
+   
+  }}
+>
+  <source src='/9928.MP4' type='video/mp4' />
+</video>
+
+          <div className='m-2'>
+            <div className='container'>
+              <div className='row'>
+                <Heading subtitle='WELCOME TO Adain Technologies' title='' />
+                <motion.p 
+                initial={{x: -1000}}
+                animate={{x: 0}}
+                transition={{
+                  duration: "2",
+                }}
+                className="text-2xl text-orange-500 "> Pioneering the future with cutting-edge<br/> AI and innovative tech solutions. Empowering businesses<br/> through intelligent automation and seamless integration.</motion.p>
+                <div className='transform translate-y-70 animate-button-animation delay-1000'>
+                  <button className='md:text-black hover:text-white  bg-orange-500 hover:bg-orange-500 md:bg:hover-black rounded-xl lg:m-8 p-1  w-42 shadow-md'>
+                    GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
+                  </button>
+                  {/* <button> VIEW ADAIN <i className='fa fa-long-arrow-alt-right'></i></button> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className='margin'></div>
+    </>
+  );
+}
+
+export default Hero;
---
+++ src/components/navbar.js
@@ -0,0 +1,163 @@
+import React from 'react';
+import Link from 'next/link';
+import { useState } from 'react';
+import Image from 'next/image';
+import { FaCaretDown } from 'react-icons/fa';
+
+
+function AdainNavBar() {
+ 
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [navbar, setNavbar] = useState(false);
+  const [activeNavItem, setActiveNavItem] = useState('');
+
+ {/* I made a function here to toggle the dropdown button whenever 
+ the user want to change the state of the dropdown to true from false... */}
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  
+   {/* I made a function here to handle what the signin button 
+  should do, you can alter it it to work with your firebase... */}
+
+  const handleClick = (navItem) => {
+    setActiveNavItem(navItem);
+    setNavbar(!navbar);
+  };
+
+  
+  return (
+    <nav className="transition-opacity duration-200 w-full top-0 h-25 shadow-lg fixed lg:mb-8 bg-white backdrop-blur-3xl hover:bg-orange-500 opacity-100 z-50 hover:bg-avista2">
+   
+     
+        <div className="justify-between px-4 mx-auto lg:max-w-8xl md:items-center md:flex sm:px-8">
+          <div className={`mt-top-adjusted ${navbar ? 'adjust-down' : ''}`}>
+            <div className="flex text-adainblack hover:text-estatelinkwhite items-center justify-between py-5">
+              <a href="/" className="flex items-center">
+              <Image src="/adain.png" width={40} height={20} alt="Avista Beach Resort Logo" /> 
+              <p className='text-sm p-2 text-adainblack font-bold tracking-widest'>Adain</p>
+              </a>
+              <div className="md:hidden text-adainblack hover:text-estatelinkwhite">
+                <button className="text-adainblack hover:text-adainwhite pt-2 rounded-md" onClick={handleClick}>
+                  {navbar ? (
+                    <svg className="text-adainblack hover:text-adainwhite icon icon-tabler icon-tabler-letter-x" fill="none" height="24" stroke="currentColor" 
+                    strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" width="24" 
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 0h24v24H0z" fill="none" stroke="none"/>
+                    <line x1="7" x2="17" y1="4" y2="20"/>
+                    <line x1="17" x2="7" y1="4" y2="20"/>
+                    </svg>
+                    
+                  ) : (
+                    <div className='text-adainblack hover:text-estatelinkwhite'>
+                    <svg width="21" height="8" viewBox="0 0 21 8" fill="none" 
+                     xmlns="http://www.w3.org/2000/svg">
+                    <rect width="21" height="2" fill="#1B1919"/>
+                   <rect y="3" width="21" height="2" fill="#1B1919"/>
+                   <rect y="6" width="21" height="2" fill="#1B1919"/>
+                   </svg>
+                   </div>
+
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+          <div>
+          <div className={` text-adainblack hover:text-adainwhite flex-2 justify-self-center pl-2 ... tracking-tight pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? "block" : "hidden"}`}>
+              <div className=" space-x-4 text-lg">
+                
+  
+  <ul className="md:px-20 mb-4 mt-2 items-center pr-20 font-medium text-sm... justify-center space-y-4 md:flex md:space-x-10 md:space-y-0">
+  <li className={`text-adainblack hover:text-adainwhite pr-16 ${activeNavItem === 'home' ? 'active' : ''}`}>
+    <Link href="/" onClick={() => handleClick('home')}>
+      {`Home`}
+    </Link>
+  </li>
+
+   {/* I made a button on Products and services here... */}
+                  
+  <li className="relative text-sm text-adainblack hover:text-adainwhite">
+    <button onClick={toggleDropdown} className="nav-link flex items-center">
+      Products
+      <FaCaretDown className="ml-1" />
+    </button>
+
+   {/* I made a conditional rendering of dropdown of the items inside the 
+   Products and services here, such that that users can switch to true or false ... */}
+
+    {isDropdownOpen && (
+      <div className="bg-orange-500 font-bold absolute py-8 my-4 text-sm  text-adainblack hover:text-adainwhite">
+        <Link href="https://quickmed.ng" className="rounded-2xl mx-2 px-2 dropdown-link  text-black hover:text-black ">
+         QuickMed
+        </Link>
+        <Link href="/" className="rounded-2xl mx-2 px-2 dropdown-link  text-black hover:text-black ">
+          Azare
+        </Link>
+        <Link href="https://adain.ng/geezfashion/" className="rounded-2xl mx-2 px-2 dropdown-link  text-black hover:text-black ">
+         Geez
+        </Link>
+        <Link href="/"className="rounded-2xl mx-2 px-2 dropdown-link  text-black hover:text-black ">
+        Needle
+        </Link>
+        </div>
+          )}
+         </li>
+
+         {/* I continued with the rest of the listed navlinks items here... */}  
+                  
+                  <li className={`text-adainblack hover:text-adainwhite ${activeNavItem === 'about' ? 'active' : ''}`}>
+                    <Link href="/About" onClick={() => handleClick('about')}>
+                      About
+                    </Link>
+                  </li>
+                  
+                  
+                  <li className={`text-adainblack hover:text-adainwhite  ${activeNavItem === 'experiences' ? 'active' : ''}`}>
+                    <Link href="/Academy" onClick={() => handleClick('experiences')}>
+                    Academy
+                    </Link>
+                  </li>
+                  <li className={`text-adainblack hover:text-adainwhite pr-20 ${activeNavItem === 'experiences' ? 'active' : ''}`}>
+                    <Link href="/Contact" onClick={() => handleClick('experiences')}>
+                    Contact
+                    </Link>
+                  </li>
+
+                 
+
+                  
+
+          
+                  
+               {/* I created a sign in button here to have an event listener of onclick 
+               that it should listen to handle click function above 
+              the parent codeblock... */}
+
+              
+                              <div className="md:text-adainblack hover:text-adainwhite  bg-adainyellow hover:bg-adainblack md:bg:hover-adainblack rounded-xl lg:m-8 p-1  w-42 shadow-md">
+                              <Link href="/signin" className="sm:px-10 text-sm pr-2 mr-4 text-lg  lg:my-20" onClick={() => handleClick('checkout')}>
+                                Sign in
+                              </Link>
+                            </div>
+            
+                
+                
+                  
+    
+    
+    
+
+</ul>
+              </div>
+            </div>
+          </div>
+        </div>
+    
+    </nav>
+  );
+}
+
+export default AdainNavBar;
