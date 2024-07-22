9afa6ed1423bcf4f4aa78ad8e9ef5d2550641cb7
Shazaniyu55 <shazaniyu@gmail.com>
2024-07-22 13:45:51 +0100

    niyunew

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+      <title>Adain Technology Ltd.</title>
+          <meta name="Adain Logo" content="...people, technologies and possibilities." />
+          <link rel="Adain Logo" href="/favicon.ico" />
+        </Head>
       <body>
         <Main />
         <NextScript />
